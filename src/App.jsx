d43ccda925ff2546c3bef5bd7c6c17a8d657dc80
Jordan Huang <jordanhuang99@gmail.com>
2018-06-13 19:14:51 +0000

    implemented broadcast to all

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,55 @@
 import React, {Component} from 'react';
 import ChatBar from './ChatBar.jsx';
 import MessageList from './MessageList.jsx';
+import { ENGINE_METHOD_DIGESTS } from 'constants';
 
 class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      currentUser: {name: "Bob"}, // optional. if currentUser is not defined, it means the user is Anonymous
-      messages: [
-        {
-          username: "Bob",
-          content: "Has anyone seen my marbles?",
-        },
-        {
-          username: "Anonymous",
-          content: "No, I think you lost them. You lost your marbles Bob. You lost them for good."
-        }
-      ]
-    }
-    this.handleChange = this.handleChange.bind(this);
+      currentUser: {name: "Bob"},
+      messages: []
+    };
+    this.addMessage = this.addMessage.bind(this);
+    this.handleUserChange = this.handleUserChange.bind(this);
   }
 
   componentDidMount() {
-    console.log("componentDidMount <App />");
-    setTimeout(() => {
-      console.log("Simulating incoming message");
-      const newMessage = {id: 3, username: "Michelle", content: "Hello there!"};
-      const messages = this.state.messages.concat(newMessage)
-      this.setState({messages: messages})
-    }, 3000);
+    this.socket = new WebSocket("ws://localhost:3001");
+    this.socket.onmessage = (event) => {
+      var new_message = JSON.parse(event.data);
+      this.setState((prev) => {
+        return {messages: [...prev.messages, new_message]}
+      })
+    };
   }
 
-  handleChange(event) {
+  addMessage(event) {
     event.preventDefault();
     if (event.key === "Enter") {
-      const new_message = {
+      var new_message = {
         username: this.state.currentUser.name,
         content: event.target.value
       };
-      this.setState((previouState) => {
-        return {
-          messages: [...previouState.messages, new_message]
-        }
-      });
+      new_message = JSON.stringify(new_message);
+      this.socket.send(new_message);
       event.target.value = "";
+      // this.setState((previouState) => {
+      //   return {
+      //     messages: [...previouState.messages, new_message]
+      //   }
+      // });
     } else {
       event.target.value += event.key;
     }
   }
 
+  handleUserChange(event) {
+    event.preventDefault();
+    this.setState({
+      currentUser: {name: event.target.value}
+    });
+  }
 
   render() {
     return (
@@ -57,7 +58,7 @@ class App extends Component {
           <a href="/" className="navbar-brand">Chatty</a>
         </nav>
         <MessageList messages={this.state.messages} />
-          <ChatBar currentUser={this.state.currentUser} onKeyPress={this.handleChange}/>
+          <ChatBar currentUser={this.state.currentUser} onKeyPress={this.addMessage} onChange={this.handleUserChange} />
       </div>
     );
   }
